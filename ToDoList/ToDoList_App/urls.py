9aea71cceafcece692cee0c199bced406dd2f545
Gug Flaviu <gfsoft03@gmail.com>
2023-06-16 23:56:41 +0300

    I fineshed the project

--- ToDoList/ToDoList_App/urls.py
+++ ToDoList/ToDoList_App/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate,TaskUpdate,DeleteTask
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask, LogIn,SingIn
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', LogIn.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'),name='logout'),
+    path('singin/',SingIn.as_view(),name='singin'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),  # se cauta de pe id
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- ToDoList/ToDoList_App/views.py
+++ ToDoList/ToDoList_App/views.py
@@ -1,32 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin # pentru a redirectiona userul la pagina de log in
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 
 
-class TaskList(ListView):
+class LogIn(LoginView):
+    template_name = 'ToDoList_App/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("tasks")
+
+
+class SingIn(FormView):
+    template_name = 'ToDoList_App/singin.html'
+    form_class = UserCreationForm
+    redirecting_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+
+        return super(SingIn, self).form_valid(form)
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(SingIn,self).get(*args,**kwargs)
+
+
+
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    def get_context_data(self, **kwargs): # pentru ca userul sa vada doar datele lui
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
 
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'  # luam toate fieldurile(titlu,descriere...) pentru a crea formul de creare
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form): # ajuta userul sa creeze taskuri pentru el nu si pentru alti useri
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteTask(DeleteView):
+
+class DeleteTask(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'tasks'
     success_url = reverse_lazy('tasks')
